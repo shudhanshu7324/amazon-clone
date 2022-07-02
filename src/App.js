c4d9400ffb1ddc93afe3bfb57331f84502341670
Shudhanshu <sudhanshubaaghi@gmail.com>
2022-07-02 21:01:04 +0530

    create the home component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       { /* Header */}
       < Header />
       { /* Home */}
+      < Home />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home_container">
+        <img
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/HSS/July/deals_3000x1200._CB634379291_.jpg"
+          alt="home-img"
+          className="home__image"
+        />
+
+        <div className="home__row">
+          {/* product */}
+          {/* product */}
+        </div>
+
+        <div className="home__row">
+          {/* product */}
+          {/* product */}
+          {/* product */}
+        </div>
+
+        <div className="home__row">
+          {/* product */}
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Home;
