7e25d129ff186dd2bb1e24fd1f12df29fb3f998a
Shudhanshu <sudhanshubaaghi@gmail.com>
2022-07-02 22:56:56 +0530

    passing props to the components

--- src/Product.js
+++ src/Product.js
@@ -1,23 +1,25 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({id , title , price , rating , image}) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>12.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-        <p>🌟</p>
+            {Array(rating).fill().map((_,i) => (
+                <p>🌟</p>
+            ))}
+        
       </div>
       </div>
 
       <img
-        src="https://images-eu.ssl-images-amazon.com/images/I/51CTIr1bJxL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg"
-        alt="product-img"
+        src={image} alt="img"
         className="product__image"
       />
 
