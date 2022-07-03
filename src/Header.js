65838b37c601b16e2617c7d9a2d568485652b810
Shudhanshu <sudhanshubaaghi@gmail.com>
2022-07-03 13:46:53 +0530

    pulled data to the header component

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,12 @@ import "./Header.css";
 import SearchTwoToneIcon from "@mui/icons-material/SearchTwoTone";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 function Header() {
+
+const [{basket},dispatch] = useStateValue();
+
   return (
     <div className="header">
       <Link to='/' style={{height: '100%'}}>
@@ -39,7 +43,7 @@ function Header() {
         >
           <div className="header__optionBasket">
             <ShoppingBasketIcon />
-            <span className="header__basketCount">0</span>
+            <span className="header__basketCount">{basket?.length}</span>
           </div>
         </Link>
       </div>
--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
-                    // taking the arguments as a props
-function Product({id , title , price , rating , image}) {     
+// taking the arguments as a props
+function Product({ id, title, price, rating, image }) {
+  // pull the data from data layer
+  const [{ basket }, dispatch] = useStateValue();
+
+  console.log("this is the basket >>>" ,basket);
+
+  const addToBasket = () => {
+    // dispatch the item to the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -12,19 +31,17 @@ function Product({id , title , price , rating , image}) {
           <strong>{price}</strong>
         </p>
         <div className="product__rating">
-            {Array(rating).fill().map((_,i) => (
-                <p>🌟</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
             ))}
-        
-      </div>
+        </div>
       </div>
 
-      <img
-        src={image} alt="img"
-        className="product__image"
-      />
+      <img src={image} alt="img" className="product__image" />
 
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
