6bfdf7566a042760f321d1da21ab703910403afd
Shudhanshu <sudhanshubaaghi@gmail.com>
2022-07-17 21:41:08 +0530

    authentication completed

--- src/App.js
+++ src/App.js
@@ -3,9 +3,35 @@ import Home from "./Home";
 import Checkout from "./Checkout";
 import Login from "./Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+
+  const [{}, dispatch] = useStateValue();
+  
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("The user is >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out.
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
     <Router>
       <div className="app">
@@ -15,7 +41,7 @@ function App() {
             element={
               <Fragment>
                 <Header />
-                <Home />{" "}
+                <Home /> 
               </Fragment>
             }
           />
@@ -25,7 +51,7 @@ function App() {
             element={
               <Fragment>
                 <Header />
-                <Checkout />{" "}
+                <Checkout />
               </Fragment>
             }
           />
--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,17 @@ import SearchTwoToneIcon from "@mui/icons-material/SearchTwoTone";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function Header() {
 
-const [{basket}] = useStateValue();
+const [{basket, user}] = useStateValue();
+
+const handleAuthentication = () => {
+  if(user) {
+    auth.signOut();
+  }
+}
 
   return (
     <div className="header">
@@ -26,10 +33,10 @@ const [{basket}] = useStateValue();
       </div>
 
       <div className="header__nav">
-        <Link to= '/login'>
-        <div className="header__option">
-          <span className="header__optionLineOne">Hello Guest</span>
-          <span className="header__optionLineTwo">SignIn</span>
+        <Link to= {!user && '/login'}>
+        <div onClick={handleAuthentication} className="header__option">
+          <span className="header__optionLineOne">Hello {!user ? 'Guest' : user.email}</span>
+          <span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In' }</span>
         </div>
         </Link>
         <div className="header__option">
